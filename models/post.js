16a6db759a1703f182b0fafead6bbb28c8cbda8b
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-11-27 14:15:54 -0800

    Modified post model

--- models/post.js
+++ models/post.js
@@ -3,13 +3,18 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Comment = require("./comment");
 
 // Mongoose model that stores each Reddit post data
-var Post = mongoose.model("Post", {
-    title: String,
-    desc: String,
-    subreddit: String, 
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+let PostSchema = new Schema({
+    title           : { type: String, required: true },
+    summary         : { type: String, required: true },
+    subreddit       : { type: String, required: true },
+    author          : { type: Schema.Types.ObjectId, ref: 'User', required: false },
+    comments        : [ Comment.schema ],
+    downVotes       : [ String ],
+    upVotes         : [ String ],
+    voteTotal       : { type: Number, default: 0 }
 });
 
-module.exports = Post;
+module.exports = mongoose.model('Post', PostSchema);
