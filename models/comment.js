1ec3a340a0f1aa89d5a9dcb27b0cef9ec07c4e96
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-11-27 14:18:16 -0800

    Modified comment model

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let CommentSchema = new Schema({
-    content: { type: String, required: true }
+    content:    { type: String, required: true },
+    author:     { type: String, required: true },
+    authorId:   { type: String, required: true },
+    comments:   [ this ]
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
